37571777b61fd9c877fc682116b86e7d24632b22
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2022-12-20 14:52:00 -0500

    Footer simple

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { BannerComponent } from './banner/banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [HeaderComponent, BannerComponent],
-  exports: [HeaderComponent, BannerComponent],
+  declarations: [HeaderComponent, BannerComponent, FooterComponent],
+  exports: [HeaderComponent, BannerComponent, FooterComponent],
   imports: [CommonModule],
 })
 export class SharedModule {}
